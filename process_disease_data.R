f9ef1a8e9101b6aad1fd4a64ca1482d86770db47
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-22 18:58:08 -0400

    Added the disease signatures from two of the new covid RNASeq datasets

--- process_disease_data.R
+++ process_disease_data.R
@@ -3,7 +3,7 @@ source("pipeline.R")
 
 cutoffs <- c(0, 0.26, 0.5)
 
-diseases <- c("influenza", "mers", "sars")
+diseases <- c("influenza", "mers", "sars", "covidc", "covidm")
 
 cell_lines <- c("HA1E", "MCF7")
 
@@ -31,6 +31,7 @@ for (pre in c(down_prefix, up_prefix)) {
 for (index in 1:length(diseases)) {
   for (cutoff in cutoffs) {
     dis <- diseases[index]
+    print(paste("Processing", dis, "at cutoff", cutoff))
     infile <- generate_name(paste(prefix, dis, sep = "/"),
                             paste(dis, "l1000", "signature", sep = "-"),
                             "tsv")
@@ -49,21 +50,22 @@ for (index in 1:length(diseases)) {
     outfile_consensus <- generate_name(HA1E_prefix,
                                        paste(dis, "l1000", "HA1E", cutoff, "consensus", "connected", sep = "-"),
                                        "tsv")
-    
-    df <- read_tsv(infile)
-    df_up <- generate_filtered_signature(df, direction = "up", threshold = cutoff)
-    write_tsv(df_up, outfile_up)
-    df_down <- generate_filtered_signature(df, direction = "down", threshold = cutoff)
-    write_tsv(df_down, outfile_down)
-    
-    up_concordant <- get_concordant_signatures(signature_df = df_up)
-    write_tsv(up_concordant, outfile_concordant_up)
-    down_concordant <- get_concordant_signatures(signature_df = df_down)
-    write_tsv(down_concordant, outfile_concordant_down)
-    
-    consensus <- generate_consensus_signature(up_concordant, down_concordant,
-                                              cell_line = "HA1E", discordant = TRUE)
-    write_tsv(consensus, outfile_consensus)
+    if (!file.exists(outfile_consensus)) {
+      df <- read_tsv(infile)
+      df_up <- generate_filtered_signature(df, direction = "up", threshold = cutoff)
+      write_tsv(df_up, outfile_up)
+      df_down <- generate_filtered_signature(df, direction = "down", threshold = cutoff)
+      write_tsv(df_down, outfile_down)
+      
+      up_concordant <- get_concordant_signatures(signature_df = df_up)
+      write_tsv(up_concordant, outfile_concordant_up)
+      down_concordant <- get_concordant_signatures(signature_df = df_down)
+      write_tsv(down_concordant, outfile_concordant_down)
+      
+      consensus <- generate_consensus_signature(up_concordant, down_concordant,
+                                                cell_line = "HA1E", discordant = TRUE)
+      write_tsv(consensus, outfile_consensus)
+    }
   }
 }
 
@@ -71,6 +73,7 @@ for (index in 1:length(diseases)) {
 for (index in 1:length(diseases)) {
   for (cutoff in cutoffs) {
     dis <- diseases[index]
+    print(paste("Processing", dis, "at cutoff", cutoff))
     infile <- generate_name(paste(prefix, dis, sep = "/"),
                             paste(dis, "l1000", "signature", sep = "-"),
                             "tsv")
@@ -90,19 +93,21 @@ for (index in 1:length(diseases)) {
                                        paste(dis, "l1000", "MCF7", cutoff, "consensus", "connected", sep = "-"),
                                        "tsv")
     
-    df <- read_tsv(infile)
-    df_up <- generate_filtered_signature(df, direction = "up", threshold = cutoff)
-    write_tsv(df_up, outfile_up)
-    df_down <- generate_filtered_signature(df, direction = "down", threshold = cutoff)
-    write_tsv(df_down, outfile_down)
-    
-    up_concordant <- get_concordant_signatures(signature_df = df_up)
-    write_tsv(up_concordant, outfile_concordant_up)
-    down_concordant <- get_concordant_signatures(signature_df = df_down)
-    write_tsv(down_concordant, outfile_concordant_down)
-    
-    consensus <- generate_consensus_signature(up_concordant, down_concordant,
-                                              cell_line = "MCF7", discordant = TRUE)
-    write_tsv(consensus, outfile_consensus)
+    if (!file.exists(outfile_consensus)) {
+      df <- read_tsv(infile)
+      df_up <- generate_filtered_signature(df, direction = "up", threshold = cutoff)
+      write_tsv(df_up, outfile_up)
+      df_down <- generate_filtered_signature(df, direction = "down", threshold = cutoff)
+      write_tsv(df_down, outfile_down)
+      
+      up_concordant <- get_concordant_signatures(signature_df = df_up)
+      write_tsv(up_concordant, outfile_concordant_up)
+      down_concordant <- get_concordant_signatures(signature_df = df_down)
+      write_tsv(down_concordant, outfile_concordant_down)
+      
+      consensus <- generate_consensus_signature(up_concordant, down_concordant,
+                                                cell_line = "MCF7", discordant = TRUE)
+      write_tsv(consensus, outfile_consensus)
+    }
   }
 }
