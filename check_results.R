59355af5988b2d7d1592d1619b34e413b46ebe79
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-07-13 16:31:46 -0400

    Completed the analysis to generate a cross table

--- check_results.R
+++ check_results.R
@@ -1,3 +1,6 @@
+# This script applies the threshold values for each consensus signature and
+# counts how many drugs are pulled up after the concordant drug search.
+
 library(tidyverse)
 
 diseases <- c("influenza", "mers", "sars", "covidc", "covidm", "dNHBE_1",
@@ -18,11 +21,29 @@ file.exists(names)
 
 data <- list()
 
+col_spec <- cols(
+  signatureid = col_character(),
+  compound = col_character(),
+  similarity = col_double()
+)
+
 for (i in 1:length(diseases)) {
   for (j in 1:length(thresholds)) {
     n <- (i*5) -(5-j)
-    data[[diseases[i]]][[as.character(thresholds[j])]] <- read_tsv(names[n])
+    data[[diseases[i]]][[as.character(thresholds[j])]] <- read_tsv(names[n], col_types = col_spec)
   }
 }
 
-lapply(data, dim)
+uldata <- unlist(data, recursive = F)
+
+counts <- as_tibble(lapply(uldata, dim))%>% 
+  t %>% 
+  as_tibble(rownames = NA) %>% 
+  rename(rows = V1, cols = V2) %>% 
+  rownames_to_column("rowname") %>% 
+  mutate(newname = sub("\\.", "/", rowname)) %>% 
+  separate(col = newname, into = c("disease", "threshold"), sep = "/") %>% 
+  select(disease, threshold, rows) %>% 
+  pivot_wider(names_from = threshold, values_from = rows)
+
+counts %>% write_csv(file.path("results", "disease_threshold_counts.csv"))
