6851d28ad542f7f5b6d1660f7e141703cd86fd26
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-08-16 03:11:53 -0400

    Cleaned up the repo further

--- find_common_signatures.R
+++
@@ -1,5 +0,0 @@
-library(tidyverse)
-
-mcf7_files <- list.files("data/L1000-Signature/connected_signatures/MCF7", pattern = "*Consensus_Connected.csv", full.names = T)
-ha1e_files <- list.files("data/L1000-Signature/connected_signatures/HA1E", pattern = "*Consensus_Connected.csv", full.names = T)
-
